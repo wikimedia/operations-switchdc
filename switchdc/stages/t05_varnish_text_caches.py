df4f08440eb695c2eb3cfb7cb7f2b2bc11268ded
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-03-20 00:40:02 +0100

    Add varnish text caches task
    Bug: T160178
    Change-Id: I19b42f46a2eeed82f1005c6d5db5ac3c9a9b2182

---
+++ switchdc/stages/t05_varnish_text_caches.py
@@ -0,0 +1,12 @@
+from switchdc import remote
+
+__title__ = 'Enable Puppet and force a run to get updated config on Varnish text caches'
+
+
+def execute(dc_from, dc_to):
+    """Enable Puppet and force a run to get updated config on Varnish text caches."""
+    rc, _ = remote.run('R:Class = Role::Cache::Text', 'async',
+                       ['puppet agent --enable', remote.get_puppet_agent_command()],
+                       batch_size=30)
+
+    return rc
