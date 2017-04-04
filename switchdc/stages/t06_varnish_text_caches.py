8cba95e7c86ffd1fb8e126cef4d2b1a992b8a85b
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-04-04 13:21:14 +0200

    Update the varnish task to use the new puppet scripts
    Change-Id: If446b9e2972a76d75f4e1899eaad786c26fa141c

--- switchdc/stages/t06_varnish_text_caches.py
+++ switchdc/stages/t06_varnish_text_caches.py
@@ -1,4 +1,3 @@
-from switchdc.lib import puppet
 from switchdc.lib.remote import Remote
 
 __title__ = 'Enable Puppet and force a run to get updated config on Varnish text caches'
@@ -8,4 +7,4 @@ def execute(dc_from, dc_to):
     """Enable Puppet and force a run to get updated config on Varnish text caches."""
     remote = Remote()
     remote.select('R:Class = Role::Cache::Text')
-    remote.async('puppet agent --enable', puppet.get_agent_run_command(), batch_size=30)
+    remote.async('enable-puppet', 'run-puppet-agent', batch_size=30)
