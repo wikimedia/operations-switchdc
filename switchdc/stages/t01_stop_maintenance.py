8bf5808b3383467bbab28e86cf80fe39e7d4e691
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-04-04 13:00:03 +0200

    Fix the stop-maintenance task
    - Do not assume anymore anything puppet does will reflect a change in
      etcd; we are disabling puppet
    - Subsequently, instead of running puppet on terbium, just brutally
      remove the crontab for www-data
    
    Change-Id: I2317ac0c4a96270820f49c1cab3f4f819c8b05d7

--- switchdc/stages/t01_stop_maintenance.py
+++ switchdc/stages/t01_stop_maintenance.py
@@ -1,6 +1,3 @@
-from switchdc import SwitchdcError
-from switchdc.lib import puppet
-from switchdc.lib.confctl import Confctl
 from switchdc.lib.remote import Remote, RemoteExecutionError
 from switchdc.log import logger
 
@@ -9,16 +6,11 @@ __title__ = "Stop MediaWiki maintenance in the old master DC"
 
 def execute(dc_from, dc_to):
     """Sets mediawiki-maintenance offline, stopping jobrunners and cronjobs."""
-    # This will make any puppet run apply the correct configuration
-    discovery = Confctl('discovery')
-    discovery.update({'pooled': False}, dnsdisc='mediawiki-maintenance',
-                     name=dc_from)
-    for obj in discovery.get(dnsdisc='mediawiki-maintenance', name=dc_from):
-        if obj.pooled:
-            logger.error("Discovery object %s should be depooled", obj.tags)
-            raise SwitchdcError(1)
+    # Note: the two steps here could be run in parallel; split the file here if
+    # deemed necessary. Since this is pre-read-only, I didn't think it would be
+    # an issue
 
-    # 2: Stop the jobrunners in dc_from
+    # 1: Stop the jobrunners in dc_from
     remote = Remote(site=dc_from)
     logger.info('Stopping jobrunners in %s', dc_from)
     jobrunners = Remote.query('R:class = role::mediawiki::jobrunner')
@@ -32,12 +24,12 @@ def execute(dc_from, dc_to):
     remote.select(jobrunners)
     remote.async('! service jobrunner status', '! service jobchron status')
 
-    # 3: disable and kill cronjobs
+    # 2: disable and kill cronjobs
     logger.info('Disabling MediaWiki cronjobs in %s', dc_from)
     remote.select('R:class = role::mediawiki::maintenance')
     remote.async(
-        puppet.get_agent_run_command(), 'killall php', 'killall php5', 'sleep 5',
-        'killall -9 php', 'killall -9 php5')
+        'crontab -u www-data -r', 'killall -r php', 'sleep 5',
+        'killall -9 -r php')
 
     # Verify that the crontab has no entries
     remote.sync('test -z "$(crontab -u www-data -l | sed -r  \'/^(#|$)/d\')"')
