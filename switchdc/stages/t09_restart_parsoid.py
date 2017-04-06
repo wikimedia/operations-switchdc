0125869679f8454e9e04d15a377fc6c33c354de9
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-04-06 09:48:58 +0200

    Rolling restart of parsoid
    Change-Id: Ie8944c8e8e2ae082d75e0a0332b67dc69e40cf4f

---
+++ switchdc/stages/t09_restart_parsoid.py
@@ -0,0 +1,11 @@
+from switchdc.lib.remote import Remote
+
+
+__title__ = "Rolling restart parsoid in eqiad and codfw"
+
+
+def execute(dc_from, dc_to):
+    from_servers = Remote(site=dc_from)
+    from_servers.sync('restart-parsoid', batch_size=1, batch_sleep=15)
+    to_servers = Remote(site=dc_to)
+    to_servers.sync('restart-parsoid', batch_size=1, batch_sleep=15)
