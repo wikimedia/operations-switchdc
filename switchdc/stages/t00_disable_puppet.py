5b4ec0ec932ff2ea01ea7b1dd416c81473f64145
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-04-10 15:15:33 +0200

    Disable puppet: fix title and docstring
    Bug: T160178
    Change-Id: I5db4317886f006a126c2d8be145d98cb86b6f1ff

--- switchdc/stages/t00_disable_puppet.py
+++ switchdc/stages/t00_disable_puppet.py
@@ -1,10 +1,11 @@
 from switchdc import get_reason
 from switchdc.lib.remote import Remote
 
-__title__ = "Stop puppet execution on maintenance, jobqueues"
+__title__ = 'Disabling puppet on MediaWiki jobrunners and videoscalers'
 
 
 def execute(dc_from, dc_to):
+    """Pre-disable puppet on all the hosts where it's needed."""
     remote = Remote()
     jobrunners = Remote.query('R:class = profile::mediawiki::jobrunner')
     maintenance = Remote.query('R:class = role::mediawiki::maintenance')
