97e28cadea090f4cddd4c5e99ac8266081c295be
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-04-11 08:20:33 +0200

    Clarify documentation for puppet disable job
    Change-Id: I8f33d06f9eb15057bdf28b58d812957b3f5269cb

--- switchdc/stages/t00_disable_puppet.py
+++ switchdc/stages/t00_disable_puppet.py
@@ -1,12 +1,13 @@
 from switchdc import get_reason
 from switchdc.lib.remote import Remote
 
-__title__ = 'Disabling puppet on MediaWiki jobrunners and videoscalers'
+__title__ = 'Disabling puppet on MediaWiki jobrunners, videoscalers and maintenace systems'
 
 
 def execute(dc_from, dc_to):
     """Pre-disable puppet on all the hosts where it's needed."""
     remote = Remote()
+    # This selects both clusters of mediawiki jobrunners: jobrunner and videoscaler
     jobrunners = Remote.query('R:class = profile::mediawiki::jobrunner')
     maintenance = Remote.query('R:class = role::mediawiki::maintenance')
     remote.select(jobrunners | maintenance)
