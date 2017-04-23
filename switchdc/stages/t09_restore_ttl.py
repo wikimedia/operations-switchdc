f49182b3a13265943cf894e533c733e3a9f82b82
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-04-24 00:23:15 +0200

    DNS: add removal of confd stale files
    Bug: T163376
    Change-Id: Ifdaa7e9e58bb596c22842fc68234efaf01a36f87

--- switchdc/stages/t09_restore_ttl.py
+++ switchdc/stages/t09_restore_ttl.py
@@ -1,14 +1,19 @@
 import time
 
 from switchdc.lib.dnsdisc import Discovery
+from switchdc.lib.remote import Remote
 
-__title__ = 'Restore the TTL of all the MediaWiki read-write discovery records'
+__title__ = 'Restore the TTL of all the MediaWiki read-write discovery records and cleanup confd stale files'
 
 
 def execute(dc_from, dc_to):
-    """Restore the original ttl on all appservers rw discovery entries"""
+    """Restore the original TTL of all the MediaWiki read-write discovery records and cleanup confd stale files."""
     ttl = Discovery('appservers-rw', 'api-rw', 'imagescaler-rw')
     ttl.update_ttl(300)
     # Verify
     time.sleep(5)
     ttl.check_ttl(300)
+
+    remote = Remote()
+    remote.select('R:class = authdns')  # Include both authdns::server and authdns::testns
+    remote.sync('rm -fv /var/run/confd-template/.discovery-{appservers,api,imagescaler}-rw.state*.err')
