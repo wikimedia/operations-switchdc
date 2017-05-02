ea76c8270cce74fe0076444391e4dd288fe4e4a7
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-05-02 13:40:06 +0200

    MediaWiki tasks: switch to use Conftool based config
    Bug: T163398
    Change-Id: I06c7861ab6a08ea99e305745d7ab9d5bc49ea031

--- switchdc/stages/t05_switch_datacenter.py
+++ switchdc/stages/t05_switch_datacenter.py
@@ -9,10 +9,10 @@ __title__ = 'Switch MediaWiki master datacenter and read-write discovery records
 
 
 def execute(dc_from, dc_to):
-    """Switched the MediaWiki master DC in etcd and in the MediaWiki code."""
-    discovery = Confctl('discovery')
+    """Switchig the MediaWiki master DC in etcd for both DNS discovery and MediaWiki config."""
     # 1: switch DNS discovery record for the new dc to on.
     # This will NOT trigger confd to change the DNS admin state as it will cause a validation error
+    discovery = Confctl('discovery')
     mw_records = '(appservers|api|imagescaler)-rw'
     discovery.update({'pooled': True}, dnsdisc=mw_records, name=dc_to)
     for obj in discovery.get(dnsdisc=mw_records, name=dc_to):
@@ -20,15 +20,10 @@ def execute(dc_from, dc_to):
             logger.error('DNS discovery record {record} is not pooled'.format(record=obj.key))
             raise SwitchdcError(1)
 
-    # 2: Deploy the MediaWiki change already merged on the deployment server in pre-flight phase
-    filename = 'CommonSettings'
-    message = 'Switch MediaWiki active datacenter to {dc_to}'.format(dc_to=dc_to)
-    expected = "$wmfMasterDatacenter = '{dc_to}';".format(dc_to=dc_to)
-    if not mediawiki.check_config_line(filename, expected):
-        mediawiki.scap_sync_config_file(filename, message)
-        if not mediawiki.check_config_line(filename, expected) and not is_dry_run():
-            logger.error('Datacenter not changed in the MediaWiki config?')
-            raise SwitchdcError(1)
+    # 2: Switch the MediaWiki config master DC
+    mediawiki.set_master_datacenter(dc_to)
+    mediawiki.check_siteinfo('.query.general["wmf-config"].wmfMasterDatacenter == "{dc_to}"'.format(dc_to=dc_to),
+                             attempts=5)
 
     # 3: switch off the old dc in conftool so that DNS discovery will be fixed
     discovery.update({'pooled': False}, dnsdisc=mw_records, name=dc_from)
