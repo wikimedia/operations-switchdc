fedaaa3ad1b2bfecbcf8acf9d457cca07f1fd268
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-05-03 08:55:26 +0000

    Revert to using scap code deploys for MediaWiki
    Our tests with EtcdConfig showed it wasn't reliable enough.
    
    This reverts commit ea76c8270cce74fe0076444391e4dd288fe4e4a7.
    
    Change-Id: I42a9bd02dd036c489330deedebaca1dd227c7156

--- switchdc/stages/t02_start_mediawiki_readonly.py
+++ switchdc/stages/t02_start_mediawiki_readonly.py
@@ -1,18 +1,36 @@
 from datetime import datetime
 
+from switchdc import SwitchdcError
+from switchdc.dry_run import is_dry_run
 from switchdc.lib import mediawiki
 from switchdc.log import irc_logger, logger
 
-__title__ = 'Set MediaWiki in read-only mode in {dc_from}'
+__title__ = 'Set MediaWiki in read-only mode in {dc_from} (db-{dc_from} config already merged and git pulled)'
 
 
 def execute(dc_from, dc_to):
-    """Set MediaWiki config readonly to True in dc_from and verify it."""
-    message = 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes'
-    mediawiki.set_readonly(message, dc=dc_from)
+    """Deploy the MediaWiki DB config for dc_from to set MediaWiki in read-only mode.
 
-    log_message = 'MediaWiki read-only period starts at: {now}'.format(now=datetime.utcnow())
-    logger.info(log_message)
-    irc_logger.info(log_message)
+    Caution: the configuration must have already merged and git pullet in the deployment host.
+    """
+    message = 'Set MediaWiki in read-only mode in datacenter {dc_from}'.format(dc_from=dc_from)
+    filename = 'db-{dc_from}'.format(dc_from=dc_from)
+    expected = """'readOnlyBySection' => [
+\t's1'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+\t's2'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+\t'DEFAULT' => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes', # s3
+\t's4'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+\t's5'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+\t's6'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+\t's7'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+],"""
 
-    mediawiki.check_siteinfo('.query.general.readonly', attempts=5)
+    if not mediawiki.check_config_line(filename, expected):
+        log_message = 'MediaWiki read-only period starts at: {now}'.format(now=datetime.utcnow())
+        logger.info(log_message)
+        irc_logger.info(log_message)
+        mediawiki.scap_sync_config_file(filename, message)
+        if not mediawiki.check_config_line(filename, expected) and not is_dry_run():
+            logger.error('Read-only mode not changed in the MediaWiki config {filename}?'.format(
+                filename=filename))
+            raise SwitchdcError(1)
--- switchdc/stages/t08_stop_mediawiki_readonly.py
+++ switchdc/stages/t08_stop_mediawiki_readonly.py
@@ -1,19 +1,36 @@
 from datetime import datetime
 
+from switchdc import SwitchdcError
+from switchdc.dry_run import is_dry_run
 from switchdc.lib import mediawiki
 from switchdc.log import irc_logger, logger
 
-__title__ = 'Set MediaWiki in read-write mode in {dc_to}'
+__title__ = 'Set MediaWiki in read-write mode in {dc_to} (db-{dc_to} config already merged and git pulled)'
 
 
 def execute(dc_from, dc_to):
-    """Set MediaWiki config readonly to False in dc_to and verify it."""
-    mediawiki.set_readonly(False, dc=dc_to)
+    """Deploy the MediaWiki DB config for dc_to to set MediaWiki in read-write mode.
 
-    mediawiki.check_siteinfo('.query.general.readonly | not', dc=dc_to, attempts=5)
+    Caution: the configuration must have already merged and git pullet in the deployment host.
+    """
+    message = 'Set MediaWiki in read-write mode in datacenter {dc_to}'.format(dc_to=dc_to)
+    filename = 'db-{dc_to}'.format(dc_to=dc_to)
+    expected = """'readOnlyBySection' => [
+#\t's1'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+#\t's2'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+#\t'DEFAULT' => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes', # s3
+#\t's4'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+#\t's5'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+#\t's6'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+#\t's7'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+],"""
 
-    log_message = 'MediaWiki read-only period ends at: {now}'.format(now=datetime.utcnow())
-    logger.info(log_message)
-    irc_logger.info(log_message)
+    if not mediawiki.check_config_line(filename, expected):
+        mediawiki.scap_sync_config_file(filename, message)
+        if not mediawiki.check_config_line(filename, expected) and not is_dry_run():
+            logger.error('Read-only mode not changed in the MediaWiki config {filename}?'.format(filename=filename))
+            raise SwitchdcError(1)
 
-    mediawiki.check_siteinfo('.query.general.readonly', dc=dc_from, attempts=5)
+        log_message = 'MediaWiki read-only period ends at: {now}'.format(now=datetime.utcnow())
+        logger.info(log_message)
+        irc_logger.info(log_message)
